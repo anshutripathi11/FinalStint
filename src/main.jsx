30deeaef98407ed4ae29ccd9cccdc107d6616c77
Anshu Tripathi <156543442+anshutripathi11@users.noreply.github.com>
2026-03-28 15:48:17 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import StudyGP from './StudyGP.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StudyGP />
-  </React.StrictMode>
-)
