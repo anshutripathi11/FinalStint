f314623d3a44291806c6d1b676cd98abdb8dd0b7
KevinMathew1709 <kevinmathew1709@gmail.com>
2026-03-28 14:31:53 -0500

    adding files

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StudyGP from './StudyGP.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StudyGP />
+  </React.StrictMode>
+)
